a7d5152b0fe7c0035a0a921a2a3ed2ef0b6a33bb
YoannDev90 <yoanndev@outlook.fr>
2024-09-27 19:59:10 +0200

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,20 @@
+# utils/helpers.py
+import discord
+from discord.ext import commands
+import asyncio
 
+async def send_to_modlogs(bot, modlogs_channel_id, embed):
+    modlogs_channel = bot.get_channel(modlogs_channel_id)
+    if modlogs_channel:
+        await modlogs_channel.send(embed=embed)
+    else:
+        print(f"Canal modlogs avec l'ID {modlogs_channel_id} non trouvé.")
+
+def create_mod_embed(action, moderator, user, reason=None):
+    embed = discord.Embed(title=f"Action de modération : {action}", color=discord.Color.red())
+    embed.add_field(name="Modérateur", value=moderator.mention, inline=False)
+    embed.add_field(name="Utilisateur", value=user.mention, inline=False)
+    if reason:
+        embed.add_field(name="Raison", value=reason, inline=False)
+    embed.set_footer(text=f"ID de l'utilisateur: {user.id}")
+    return embed
